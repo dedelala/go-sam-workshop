b6b535f0bc261e354021a9455b281e3d83daf551
Dede Lamb <dede.lamb@myob.com>
2018-09-03 19:23:49 +1000

    count words!

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dedelala/go-sam-workshop/wc
+module github.com/dedelala/go-sam-workshop
 
 require github.com/aws/aws-lambda-go v1.6.0
--- main.go
+++ main.go
@@ -1,18 +1,49 @@
-package main // import "github.com/dedelala/go-sam-workshop/wc"
+package main // import "github.com/dedelala/go-sam-workshop"
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
+	"unicode"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	rsp := events.APIGatewayProxyResponse{
-		Body:       "blep",
-		StatusCode: http.StatusOK,
+// words counts the number of continuous runs of non-space runes in s.
+func words(s string) int {
+	b, n := true, 0
+	for _, r := range []rune(s) {
+		switch {
+		case unicode.IsSpace(r):
+			b = true
+		case b:
+			b = false
+			n++
+		}
 	}
-	return rsp, nil
+	return n
+}
+
+// respond returns a handler response in a slightly less verbose way.
+func respond(code int, msg string) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       msg,
+		StatusCode: code,
+	}, nil
+}
+
+func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var b bytes.Buffer
+
+	msg := map[string]int{"words": words(req.Body)}
+
+	if err := json.NewEncoder(&b).Encode(msg); err != nil {
+		return respond(http.StatusInternalServerError,
+			`{"error":"failed to encode response"}`)
+	}
+
+	return respond(http.StatusOK, b.String())
 }
 
 func main() {
